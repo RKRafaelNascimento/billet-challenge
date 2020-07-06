128c0965a40fd6d57afc6441216eb4cf909eff0c
RKRafaelNascimento <RKRafael.nascimento@gmail.com>
2020-07-05 23:11:14 -0300

    Adicionado AbstractBarCode

---
+++ src/abstracts/AbstractBarCode.ts
@@ -0,0 +1,10 @@
+import { IBarCodeInformation } from 'src/interfaces/IBarCodeInformation.interface';
+
+export abstract class AbstractBarCode {
+    public abstract getBarCodeInformation(typedLine: string): IBarCodeInformation;
+    protected abstract isCorrectFormat(typedLine: string): boolean;
+    protected abstract isValidCheckDigits(barcode: string): boolean;
+    protected abstract convertBarCode(barcode: string): string;
+    protected abstract getValueInBarCode(barcode: string);
+    protected abstract getDueDateInBarCode(barcode: string): string;
+}
