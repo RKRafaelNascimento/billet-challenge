c32c17b85d0f70641b92100da2560fd4c4585c33
RKRafaelNascimento <RKRafael.nascimento@gmail.com>
2020-07-06 18:46:48 -0300

    Ajustes

--- src/abstracts/AbstractBarCode.ts
+++ src/abstracts/AbstractBarCode.ts
@@ -1,7 +1,7 @@
 import { IBarCodeInformation } from 'src/interfaces/IBarCodeInformation.interface';
 
 export abstract class AbstractBarCode {
-    public abstract getBarCodeInformation(typedLine: string): IBarCodeInformation;
+    public abstract  getBarCodeInformation(typedLine: string): IBarCodeInformation;
     protected abstract isCorrectFormat(typedLine: string): boolean;
     protected abstract isValidCheckDigits(barcode: string): boolean;
     protected abstract convertBarCode(barcode: string): string;
